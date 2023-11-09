7aa90911c974e4525be75546bde09e4fe7667f49
Lucas Bargas <lucas.bargasoficial@gmail.com>
2023-11-09 00:47:48 -0300

    Modal legend - updated

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -11,7 +11,7 @@ export const Title = (props: Props): JSX.Element => {
   return (
     <S.TitleContainer fontSize={props.fontSize}>
       {props.mainTitle ? <h1>{props.title}</h1> : <h2>{props.title}</h2>}
-      {!!props.legend && <span>"{props.legend}"</span>}
+      {!!props.legend && <span>{props.legend}</span>}
     </S.TitleContainer>
   );
 };
